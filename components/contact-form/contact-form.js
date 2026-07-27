159281d942993d7d727fe9007c0234faf4c69efc
Waqas Hakim <162884901+Waqashakim1122@users.noreply.github.com>
2026-07-27 13:35:59 +0500

    Delete components directory

--- components/contact-form/contact-form.js
+++
@@ -1,6 +0,0 @@
-// CONTACT FORM component behaviour
-function handleContactSubmit(e){
-  e.preventDefault();
-  document.getElementById('contact-success').style.display = 'block';
-  document.getElementById('contact-form').querySelectorAll('input, textarea').forEach(el => el.value = '');
-}
